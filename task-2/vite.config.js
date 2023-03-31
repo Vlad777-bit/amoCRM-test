4dacd10a113e010bcc10104eb38fa84afa6b9511
Vladislav Nozhkin <mega.nozhkin@pm.me>
2023-03-31 11:36:41 +0300

    init react app

---
+++ task-2/vite.config.js
@@ -0,0 +1,22 @@
+import { URL, fileURLToPath } from 'url';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    svgr({
+      exportAsDefault: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    port: 3000,
+  },
+});
